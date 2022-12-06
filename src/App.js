77a0cd7bfbda6cc6cd012638c7626b3480db8d9d
Ilyas <kz.smile@bk.ru>
2022-12-06 16:37:29 +0600

    Состояния компонентов

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
-import logo from './logo.svg';
+import {Component} from 'react';
 import './App.css';
 
 // props (пропсы) — это входные данные React-компонентов, передаваемые от родительского компонента дочернему компоненту.
-function WhoAmI({name, surname, link}) {
-	return (
-		<div>
-			<h1>My name is {name()}, surname - {surname}</h1>
-			<a href={link}>My profile</a>
-		</div>
-	)
+class WhoAmI extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			years: 27,
+			text: '+++'
+		}
+	}
+
+	nextYear = () => {  // Только стрелочная функция
+		console.log('+++');
+		this.setState(state => ({ //State на прямую менять нельзя только так.
+			years: state.years + 1
+		}))
+	}
+
+	render() {
+		const {name, surname, link} = this.props
+		return (
+			<div>
+				<button onClick={this.nextYear}>{this.state.text}</button>
+				<h1>My name is {name}, surname - {surname}, age - {this.state.years}</h1>
+				<a href={link}>My profile</a>
+			</div>
+		)
+	}
 }
 
 function App() {
 	return (
 		<div className='App'>
-			<WhoAmI name={() => {return 'John'}} surname='Smith' link='facebook.com'/>
-			<WhoAmI name={() => {return 'Alex'}} surname='Shepard' link='vk.com'/>
+			<WhoAmI name='John' surname='Smith' link='facebook.com'/>
+			<WhoAmI name='Alex' surname='Shepard' link='vk.com'/>
 		</div>
 	)
 }
